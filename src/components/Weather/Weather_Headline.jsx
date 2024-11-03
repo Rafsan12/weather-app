3117f64761e88053fe29409f5bb1d1dcfc5e975a
Rafsan12 <rafsan.web.2022@gmail.com>
2024-11-03 15:19:30 +0600

    show dynamic icon based on weather

--- src/components/Weather/Weather_Headline.jsx
+++ src/components/Weather/Weather_Headline.jsx
@@ -1,25 +1,68 @@
 import { useContext } from "react";
 import PinSvg from "../../assets/pin.svg";
+
+import ClodIcon from "../../assets/cloud.svg";
+import HazeIcon from "../../assets/haze.svg";
+import ShowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
+import RainIcon from "../../assets/rainy.svg";
+import ThunderIcon from "../../assets/thunder.svg";
+
 import { WeatherContext } from "../../context";
+import { getFormatDate } from "../../utlis/date";
 
 export default function Weather_Headline() {
   const { weatherData } = useContext(WeatherContext);
+  const { location, climate, temperature, time } = weatherData;
+
+  function getWeatherIcon(climate) {
+    switch (climate) {
+      case "Rain":
+        return RainIcon;
+
+      case "Thunder":
+        return ThunderIcon;
+
+      case "Clouds":
+        return ClodIcon;
+
+      case "Clear":
+        return SunnyIcon;
+
+      case "Show":
+        return ShowIcon;
+
+      case "Haze":
+        return HazeIcon;
+      case "Fog":
+        return HazeIcon;
+      case "Mist":
+        return HazeIcon;
+
+      default:
+        return SunnyIcon;
+    }
+  }
+
   return (
     <>
       <div>
         <div className="max-md:flex items-center justify-between md:-mt-10">
-          <img src="../../assets/cloud.svg" alt="cloud" />
+          <img src={getWeatherIcon(climate)} alt="cloud" />
           <div className="max-md:flex items-center max-md:space-x-4">
             <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-              {weatherData.temperature}°
+              {Math.round(temperature)}°
             </h1>
             <div className="flex items-center space-x-4 md:mb-4">
               <img src={PinSvg} />
-              <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+              <h2 className="text-2xl lg:text-[50px]">{location}</h2>
             </div>
           </div>
         </div>
-        <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+        <p className="text-sm lg:text-lg">
+          {getFormatDate(time, "time", false)} -{" "}
+          {getFormatDate(time, "date", false)}
+        </p>
       </div>
     </>
   );
